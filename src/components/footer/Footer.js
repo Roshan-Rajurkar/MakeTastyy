be558977b14a43af45cfa45c4f8632beeb69432e
Roshan-Rajurkar <roshangit50@gmail.com>
2023-05-19 19:28:12 +0530

    adding the social media

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -34,7 +34,7 @@ const Footer = () => {
             <div className="feedback-form">
                 <form onSubmit={handleFeedbackSubmit}>
                     <input type="email" name="name" placeholder="Your Email" required onChange={(e) => setEmail(e.target.value)} />
-                    <textarea name="message" placeholder="Your Feedback" required onChange={(e) => setMessage(e.target.value)}></textarea>
+                    <textarea name="message" placeholder="Your Feedback" required value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
                     <button type="submit" className='send_feedback'><span>Send Feedback</span> <FiChevronRight className='send_icon' /> </button>
                 </form>
             </div>
@@ -46,10 +46,10 @@ const Footer = () => {
                     <p className="footer-text">© 2023 MakeTastyy. All rights reserved.</p>
                 </div>
                 <div className="footer-right">
-                    <a href="https://roshan-rajurkar.github.io/R_R_/" target="_blank" className="social-icon"><FaExternalLinkAlt /></a>
-                    <a href="https://www.instagram.com/roshan_rajurkar__/" target="_blank" className="social-icon"><FaInstagram /></a>
-                    <a href="https://github.com/Roshan-Rajurkar" target="_blank" className="social-icon"><FaGithub /></a>
-                    <a href="https://www.linkedin.com/in/roshan-rajurkar-3400a4206/" target="_blank" className="social-icon"><FaLinkedin /></a>
+                    <a href="https://roshan-rajurkar.github.io/R_R_/" target="_blank" rel='noreferrer' className="social-icon"><FaExternalLinkAlt /></a>
+                    <a href="https://www.instagram.com/roshan_rajurkar__/" target="_blank" rel='noreferrer' className="social-icon"><FaInstagram /></a>
+                    <a href="https://github.com/Roshan-Rajurkar" target="_blank"  rel='noreferrer' className="social-icon"><FaGithub /></a>
+                    <a href="https://www.linkedin.com/in/roshan-rajurkar-3400a4206/" target="_blank" rel='noreferrer' className="social-icon"><FaLinkedin /></a>
                 </div>
             </div>
         </footer>
--- src/components/recipeCard/RecipeCard.js
+++ src/components/recipeCard/RecipeCard.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from '@mui/material';
+import { Button, Dialog, DialogTitle, DialogContent, DialogActions, DialogContentText } from '@mui/material';
 import './recipeCard.css';
 
 const RecipeCard = ({ recipe }) => {
   const [open, setOpen] = useState(false);
+  const [popupType, setPopupType] = useState('ingredients'); // Track the type of popup to open
   const [videoId, setVideoId] = useState('');
 
-  const handleOpen = () => {
+  const handleOpen = (type) => {
     setOpen(true);
+    setPopupType(type);
   };
 
   const handleClose = () => {
@@ -16,24 +18,26 @@ const RecipeCard = ({ recipe }) => {
 
   useEffect(() => {
     const fetchVideo = async () => {
-      const searchQuery = encodeURIComponent(`${recipe.label} recipe`);
-      const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${searchQuery}&key=AIzaSyD9QvfKHF2jg-YdkKvPikP6HM0pNBelq58`;
+      if (!videoId) {
+        const searchQuery = encodeURIComponent(`${recipe.label} recipe`);
+        const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${searchQuery}&key=AIzaSyBZAYCz9HxKtQE72PqyR4JjmM2iQNQ0nHE`;
 
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log(data)
-        // if (data.items.length > 0) {
-        //   const videoId = data.items[0].id.videoId;
-        //   setVideoId(videoId);
-        // }
-      } catch (error) {
-        console.error('Error fetching video:', error);
+        try {
+          const response = await fetch(url);
+          const data = await response.json();
+          if (data.items.length > 0) {
+            const videoId = data.items[0].id.videoId;
+            setVideoId(videoId);
+          }
+        } catch (error) {
+          console.log("No call are making")
+          console.error('Error fetching video:', error);
+        }
       }
     };
 
     fetchVideo();
-  }, [recipe.label]);
+  }, [recipe.label, videoId]);
 
   return (
     <div className='recipe_card'>
@@ -42,21 +46,38 @@ const RecipeCard = ({ recipe }) => {
         {recipe.label.split(' ').slice(0, 5).join(' ')}
       </h3>
       <div className="recipe_car_buttons">
-        <button className='recipe_ingredients' onClick={handleOpen}>Ingredients</button>
-        { (
-          <button onClick={() => window.open(`https://www.youtube.com/watch?v=${videoId}`)} className='recipe_more_details'>Get Cooking</button>
+        <button className='recipe_ingredients' onClick={() => handleOpen('ingredients')}>
+          Ingredients
+        </button>
+        {videoId && (
+          <button className='recipe_more_details' onClick={() => handleOpen('video')}>
+            Get Cooking
+          </button>
         )}
       </div>
 
-      {/* dialog Box using the materialUI */}
+      {/* Dialog Box for Ingredients and Video */}
       <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Ingredients</DialogTitle>
+        <DialogTitle>{popupType === 'ingredients' ? 'Ingredients' : 'Video'}</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            {recipe.ingredients.map((ingredient, index) => (
-              <div key={index}>{index + 1}. {ingredient.text}</div>
-            ))}
-          </DialogContentText>
+          {popupType === 'ingredients' ? (
+            <DialogContentText>
+              {recipe.ingredients.map((ingredient, index) => (
+                <div key={index}>{index + 1}. {ingredient.text}</div>
+              ))}
+            </DialogContentText>
+          ) : (
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <iframe
+                title="Recipe Video"
+                width="560"
+                height="315"
+                src={`https://www.youtube.com/embed/${videoId}`}
+                frameBorder="0"
+                allowFullScreen
+              ></iframe>
+            </div>
+          )}
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Close</Button>
